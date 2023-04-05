dfdcf5faed6bafc927060bd6d298b93247f972ea
dhrutibhudia97 <dhruti.bhudia@outlook.com>
2023-04-05 11:58:45 +0000

    add es7 snippets

---
+++ my-app/src/components/ES7Snippets.js
@@ -0,0 +1,55 @@
+// // type rfce...
+// import React from 'react'
+
+// function ES7Snippets() {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// // type rafce...
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//   return (
+//     <div>ES7Snippets</div>
+//   )
+// }
+
+// export default ES7Snippets
+
+
+// // type rce...
+// import React, { Component } from 'react'
+
+// export class ES7Snippets extends Component {
+//   render() {
+//     return (
+//       <div>ES7Snippets</div>
+//     )
+//   }
+// }
+
+// export default ES7Snippets
+
+
+// //type rconst...
+// constructor(props) {
+//   super(props)
+
+//   this.state = {
+//      first
+//   }
+// }
+
+
+
+// //type clg... to log something to the console.
+// console.log(first)
+
+
+// // type clo... to log object with its name as a string
+// console.log('first', first)
